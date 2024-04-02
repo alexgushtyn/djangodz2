30c3d465143e22c9206a762fda1a9238b824bb9a
Alex Gushtyn <sashabaltnk@gmail.com>
2024-04-02 23:57:35 +0300

    12222

--- catalog/models.py
+++ catalog/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     pur_price = models.DecimalField(max_digits=1000, decimal_places=0, verbose_name='Цена за покупку')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания(записи в БД)')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения(записи в БД')
-    manufactured_at = models.DateTimeField(auto_now=True, verbose_name='Дата производства продукта')
+
 
     def __str__(self):
         return f'{self.name} ({self.category}'
